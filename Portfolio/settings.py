28eb058d009f60cd80c66ad5b68efc23c19c5a90
Alexandr Kononuk <49821742+mccuyper@users.noreply.github.com>
2021-08-10 13:21:45 +0300

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app', 'static'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -128,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mccuyper@gmail.com'
-EMAIL_HOST_PASSWORD = 'bgdkdhcpobydzvnv'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
